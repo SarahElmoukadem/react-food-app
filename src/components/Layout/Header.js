94b65ef3513b3473cff0df7870899aeacea5fd10
Sarah Elmoukadem <SarahElmoukadem@users.noreply.github.com>
2022-04-16 03:07:54 -0700

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,24 @@
-import classes from './Header.module.css'
-import mealsImage from '../../assets/meals.jpg'
-import HeaderCartBtn from './HeaderCartBtn';
-const Header = (props) => {
-return(<>
-<header className={classes.header}>
-    <h1>Restaurant Meals</h1>
-    <HeaderCartBtn onClick={props.onShowCart}/>
-</header>
+import React from 'react'
+import mealsImg from '../../assets/meals.jpg'
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
-<div className={classes['main-image']}>
-    <img src={mealsImage} alt='img meals'/>
-</div>
-</>)
+
+const Header = (props) => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    ReactMeals
+                </h1>
+                <HeaderCartButton onClick={props.onShowCart}/>
+            </header>
+            <div className={classes['main-image']}>
+
+            <img src={mealsImg} alt="Meals image"/>
+            </div>
+        </>
+    )
 }
 
-export default Header;
+export default Header
