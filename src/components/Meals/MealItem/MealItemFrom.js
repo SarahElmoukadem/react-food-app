6c30750b3c35ffca0ff5ed7fb5a79438639904ad
Sarah Elmoukadem <SarahElmoukadem@users.noreply.github.com>
2022-04-16 02:50:43 -0700

    Update MealItemFrom.js

--- src/components/Meals/MealItem/MealItemFrom.js
+++ src/components/Meals/MealItem/MealItemFrom.js
@@ -1,48 +1,44 @@
-import Input from '../../UI/Input';
-import classes from './MealItemFrom.module.css'
-
 import React, { useRef, useState } from 'react'
+import classes from './MealItemForm.module.css';
+import Input from '../../UI/Input';
 
-const MealItemFrom = (props) => {
+const MealItemForm = (props) => {
     const [amountIsValid, setAmountIsValid] = useState(true)
+    const amountInputRef = useRef();
 
-    const amountInputRef = useRef()
-    const submitHandler = (event) => {
-        event.preventDefault()
-        const amountValue = amountInputRef.current.value;
-        const amountNum = +amountValue;
-
-        if (amountValue.trim().length === 0 ||
-            amountValue.trim().length < 1 ||
-            amountValue.trim().length > 5) {
+    const submitHandler = e => {
+        e.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+        if (enteredAmount.trim().length === 0 || enteredAmount.trim().length < 1 || enteredAmount.trim().length > 5) {
             setAmountIsValid(false)
             return;
         }
 
-        props.onAddToCart(amountNum)
+        props.onAddToCart(enteredAmountNumber)
     }
-    return (
-        <form
-            className={classes.form}
-            onSubmit={submitHandler}
 
-        >
-            <Input input={
-                {
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label="Amount"
+                input={{
                     id: props.id,
                     type: 'number',
                     min: '1',
-                    max: '5',
-                    defaultValue: '1',
-                    step: '1'
-                }}
-                label="Amount"
-                ref={amountInputRef} />
-            <button>+ Add</button>
-
-            {!amountIsValid && <p>Amount isn't valid</p>}
+                    max: '1',
+                    step: '1',
+                    defaultValue: '1'
+                }} />
+            <button>
+                + Add
+            </button>
+            {!amountIsValid && <p>
+                Please enter a valid amount (1-5).
+            </p>}
         </form>
     )
 }
 
-export default MealItemFrom;
+export default MealItemForm
